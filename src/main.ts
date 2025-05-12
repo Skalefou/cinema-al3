2d62bbf17c74ee66e2325f6cd57ae32c21d6a2e7
Skalefou <skamsagar@gmail.com>
2025-05-12 11:59:05 +0200

    Swagger

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,29 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { RolesGuard } from './auth/guards/roles.guard';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
 
-    app.useGlobalPipes(new ValidationPipe());
-      const jwtAuthGuard = app.get(JwtAuthGuard);  // ✅ fonctionne maintenant
-      const rolesGuard = app.get(RolesGuard);
+  // Swagger config
+  const config = new DocumentBuilder()
+    .setTitle('Cinema API')
+    .setDescription('Documentation interactive de l’API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
 
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // Pipes et guards globaux
+  app.useGlobalPipes(new ValidationPipe());
+  const jwtAuthGuard = app.get(JwtAuthGuard);
+  const rolesGuard = app.get(RolesGuard);
   app.useGlobalGuards(jwtAuthGuard, rolesGuard);
 
-    await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 
 bootstrap();
